67ad782221a92ae725d117fea276d399a6f6fa49
Ibiyemi Abiodun <ibiyemi@google.com>
2020-05-29 20:07:38 -0400

    Use classes for theme switching

--- portfolio/src/main/client/src/components/controls/theme-switcher.ts
+++ portfolio/src/main/client/src/components/controls/theme-switcher.ts
@@ -2,7 +2,8 @@ import '@style/index.dark.scss';
 import '@style/index.light.scss';
 import {htmlElement} from '@src/util/html';
 
-document.body.setAttribute('data-theme', 'light');
+// not adding this in HTML for encapsulation
+document.body.classList.add('theme-light');
 
 /**
  * Toggles the theme between light and dark.
@@ -11,9 +12,12 @@ export function toggle(): void {
   // sample because we don't want this to run again
   // in response to the theme being changed, that leads
   // to infinite loop
-  if (document.body.getAttribute('data-theme') === 'light') {
-    document.body.setAttribute('data-theme', 'dark');
+  if (document.body.classList.contains('theme-light')) {
+    document.body.classList.remove('theme-light');
+    document.body.classList.add('theme-dark');
   } else {
+    document.body.classList.remove('theme-dark');
+    document.body.classList.add('theme-light');
     document.body.setAttribute('data-theme', 'light');
   }
 }
