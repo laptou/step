4ad3c3497a57688f0072717717ad1ab0cfd42ff8
Ibiyemi Abiodun <ibiyemi@intulon.com>
2020-05-30 21:35:44 -0400

    Add auto dark mode

--- portfolio/src/main/client/src/components/controls/theme-switcher.ts
+++ portfolio/src/main/client/src/components/controls/theme-switcher.ts
@@ -1,24 +1,36 @@
 import {htmlElement} from '@src/util/html';
 
-// not adding this in HTML for encapsulation
-document.body.classList.add('theme-light');
+const now = new Date();
+if (now.getHours() >= 21 || now.getHours() < 9) {
+  // if it is before 9:00 or after 21:00, use dark theme automatically
+  setTheme('dark');
+} else {
+  setTheme('light');
+}
+
+/**
+ * @param theme The theme to apply.
+ */
+export function setTheme(theme: string): void {
+  document.body.classList.remove('theme-light');
+  document.body.classList.remove('theme-dark');
+  document.body.classList.add(`theme-${theme}`);
+}
 
 /**
  * Toggles the theme between light and dark.
  */
-export function toggle(): void {
+export function toggleTheme(): void {
   if (document.body.classList.contains('theme-light')) {
-    document.body.classList.remove('theme-light');
-    document.body.classList.add('theme-dark');
+    setTheme('dark');
   } else {
-    document.body.classList.remove('theme-dark');
-    document.body.classList.add('theme-light');
+    setTheme('light');
   }
 }
 
 export const ThemeSwitcher = (): HTMLElement => {
   const btn: HTMLButtonElement =
     htmlElement`<button class='theme-switcher'>Switch theme</button>`;
-  btn.onclick = toggle;
+  btn.onclick = toggleTheme;
   return btn;
 };
