4253dfeaedf3bc69838a855a6fa4223606747834
Ibiyemi Abiodun <ibiyemi@google.com>
2020-06-01 15:23:04 -0400

    Add fetch

--- portfolio/src/main/client/src/index.ts
+++ portfolio/src/main/client/src/index.ts
@@ -5,3 +5,9 @@ import '@style/index.scss';
 
 document.body.append(...IndexPage());
 document.body.appendChild(Lightbox());
+
+(async () => {
+  const result = await fetch('/data');
+  const text = await result.text();
+  document.body.append(text);
+})().catch((err) => console.error(err));
