deba15296c0b1999ec078827532fbde099cd7c6d
Ibiyemi Abiodun <ibiyemi@google.com>
2020-06-02 17:53:01 -0400

    Fix merge error

--- portfolio/src/main/client/src/components/controls/lightbox.ts
+++ portfolio/src/main/client/src/components/controls/lightbox.ts
@@ -47,10 +47,6 @@ const lightbox: HTMLDivElement = htmlElement`
   </div>`;
 
 lightbox.addEventListener('click', hideLightbox);
-window.addEventListener('keydown', (e) => {
-  if (e.keyCode !== 27) return;
-  hideLightbox();
-});
 
 window.addEventListener('keydown', (e) => {
   if (e.key !== 'Escape') return;
