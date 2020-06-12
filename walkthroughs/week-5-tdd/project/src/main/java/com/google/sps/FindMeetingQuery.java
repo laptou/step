1d1fe35d8a8d024a57d62cd88813ae8d6e71fbf3
Ibiyemi Abiodun <ibiyemi@google.com>
2020-06-12 17:41:03 -0400

    All tests pass

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,6 +15,7 @@
 package com.google.sps;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -22,16 +23,16 @@ import java.util.List;
 import java.util.Queue;
 
 public final class FindMeetingQuery {
-  private static class TimeSegment {
+  private static class TimeTree {
     public final TimeRange range;
-    public final TimeSegment left;
-    public final TimeSegment right;
+    public final TimeTree left;
+    public final TimeTree right;
 
-    public TimeSegment(TimeRange range) {
+    public TimeTree(TimeRange range) {
       this(range, null, null);
     }
 
-    public TimeSegment(TimeRange range, TimeSegment left, TimeSegment right) {
+    public TimeTree(TimeRange range, TimeTree left, TimeTree right) {
       this.range = range;
       this.left = left;
       this.right = right;
@@ -44,13 +45,22 @@ public final class FindMeetingQuery {
   }
 
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    TimeSegment root = new TimeSegment(TimeRange.WHOLE_DAY, null, null);
-    TimeSegment optionalRoot = root;
 
     System.out.printf("\nrequesting %d minute meeting\n", request.getDuration());
 
     if (request.getDuration() > 1440)
       return Collections.emptyList();
+
+    TimeTree root = new TimeTree(TimeRange.WHOLE_DAY, null, null);
+
+    boolean hasOptionalAttendees = !request.getOptionalAttendees().isEmpty();
+    boolean hasRequiredAttendees = !request.getAttendees().isEmpty();
+
+    if (!hasOptionalAttendees && !hasRequiredAttendees) {
+      return Arrays.asList(root.range);
+    }
+
+    TimeTree optionalRoot = hasOptionalAttendees ? root : null;
 
     for (Event event : events) {
       for (String attendee : event.getAttendees()) {
@@ -59,16 +69,20 @@ public final class FindMeetingQuery {
 
         if (request.getAttendees().contains(attendee)) {
           System.out.printf("removing blocker %s\n", blocked);
+
           // this is a blocker, split up the available time segment
           root = removeRange(root, blocked, buffer);
           optionalRoot = removeRange(optionalRoot, blocked, buffer);
+
           System.out.printf("root: %s\n", root);
           System.out.printf("opt: %s\n", optionalRoot);
         }
 
         if (request.getOptionalAttendees().contains(attendee)) {
           System.out.printf("removing optional %s\n", blocked);
+
           optionalRoot = removeRange(optionalRoot, blocked, buffer);
+
           System.out.printf("opt: %s\n", optionalRoot);
         }
       }
@@ -76,19 +90,18 @@ public final class FindMeetingQuery {
 
     // traverse time segments
     List<TimeRange> availableRanges = new ArrayList<>();
-    Queue<TimeSegment> toProcess = new LinkedList<>();
+    Queue<TimeTree> toProcess = new LinkedList<>();
 
     // if anything remained while including optional attendees, get them
-    if (optionalRoot != null) {
+    if (hasOptionalAttendees && optionalRoot != null) {
       toProcess.add(optionalRoot);
-    } else if (root != null) {
+    } else if (hasRequiredAttendees && root != null) {
       toProcess.add(root);
     }
-
     while (!toProcess.isEmpty()) {
-      TimeSegment seg = toProcess.poll();
+      TimeTree seg = toProcess.poll();
 
-        // extend the blocked zone forwards by the duration of the request
+      // extend the blocked zone forwards by the duration of the request
       if (seg.left == null && seg.right == null) {
         availableRanges.add(seg.range);
         continue;
@@ -109,7 +122,7 @@ public final class FindMeetingQuery {
   /**
    * Removes the range `toRemove` from the segment `segment`, splitting it if necessary.
    */
-  private TimeSegment removeRange(TimeSegment segment, TimeRange toRemove, long buffer) {
+  private TimeTree removeRange(TimeTree segment, TimeRange toRemove, long buffer) {
     if (segment == null || !segment.range.overlaps(toRemove))
       return segment;
 
@@ -126,16 +139,16 @@ public final class FindMeetingQuery {
     boolean hasChild = false;
 
     if (segment.left != null) {
-      TimeSegment newLeft = removeRange(segment.left, toRemove, buffer);
+      TimeTree newLeft = removeRange(segment.left, toRemove, buffer);
       hasChild = newLeft != null;
-      segment = new TimeSegment(baseRange, newLeft, segment.right);
-    } 
+      segment = new TimeTree(baseRange, newLeft, segment.right);
+    }
 
     if (segment.right != null) {
-      TimeSegment newRight = removeRange(segment.right, toRemove, buffer);
+      TimeTree newRight = removeRange(segment.right, toRemove, buffer);
       hasChild = hasChild || newRight != null;
-      segment = new TimeSegment(baseRange, segment.left, newRight);
-    } 
+      segment = new TimeTree(baseRange, segment.left, newRight);
+    }
 
     if (hasChild) {
       return segment;
@@ -143,16 +156,16 @@ public final class FindMeetingQuery {
 
     if (spaceAtStart && !spaceAtEnd) {
       TimeRange newRange = TimeRange.fromStartEnd(baseRange.start(), toRemove.start(), false);
-      return new TimeSegment(newRange, segment.left, segment.right);
+      return new TimeTree(newRange, segment.left, segment.right);
     }
 
     if (!spaceAtStart && spaceAtEnd) {
       TimeRange newRange = TimeRange.fromStartEnd(toRemove.end(), baseRange.end(), false);
-      return new TimeSegment(newRange, segment.left, segment.right);
+      return new TimeTree(newRange, segment.left, segment.right);
     }
 
     TimeRange newLeft = TimeRange.fromStartEnd(baseRange.start(), toRemove.start(), false);
     TimeRange newRight = TimeRange.fromStartEnd(toRemove.end(), baseRange.end(), false);
-    return new TimeSegment(baseRange, new TimeSegment(newLeft), new TimeSegment(newRight));
+    return new TimeTree(baseRange, new TimeTree(newLeft), new TimeTree(newRight));
   }
 }
